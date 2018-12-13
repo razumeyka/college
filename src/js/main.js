79e770060f41b10e95bd73bd7b18d32010e56204
razumeyka <33911265+razumeyka@users.noreply.github.com>
2018-12-14 01:06:38 +0200

    corrections

--- src/js/main.js
+++ src/js/main.js
@@ -6,36 +6,36 @@ $(document).ready(function(){
 // mobile_menu
     
     $('.burger').click( function() { 
-        $('.menu-line .menu').slideToggle(300);
+        $('.header2 .menu').slideToggle(300);
         $('.burger').toggleClass( 'burger_active' ); 
     });
+    
+// sub-menu 1280
+    
+    if ( $(window).width() > 1279 ) {
+        $('.menu li').mouseenter(function () { 
+            $(this).find('.sub-menu').fadeIn(200).addClass('active'); 
+        });
 
-// sub-menu 
-    
+        $('.menu li').mouseleave(function () { 
+            $(this).find('.sub-menu').fadeOut(200).removeClass('active'); 
+        });
+    };
+
+// sub-menu 1279
+
     $('.menu li.menu-item-has-children>a').click(function(e){
-		e.preventDefault();
-		if(window.innerWidth>1279)return ;
-		$('.sub-menu').not($(this).closest('li').find('.sub-menu')).slideUp('300');
-		$(this).closest('li').find('.sub-menu').slideToggle('300');
+        if($('.burger').is(':visible')){
+            e.preventDefault();
+            $(this).closest('li').find('.sub-menu').slideToggle(300);
+        }
     }); 
-    
-    $('.menu li.sub-menu-item-has-children>a').click(function(e){
-        e.preventDefault();
-		if(window.innerWidth>1279)return ;
-        $('.sub-menu ul').not($(this).closest('li').find(' ul')).slideUp('300');	$(this).closest('li').find('ul:first').slideToggle('300');
-    }); 
-    
-    $('.menu li.small-sub-menu-item-has-children>a').click(function(e){
-        e.preventDefault();
-		if(window.innerWidth>1279)return ;
-        $('.sub-menu ul').not($(this).closest('li').find(' ul')).not($(this).closest('li').closest('ul')).slideUp('300');	$(this).closest('li').find('ul:first').slideToggle('300');
-    });
 
 // hidden text
-    $(".second_part").slideUp(0);
+    $(".info-block__second_part").slideUp(0);
     $(".more-info").click(function(event){
         event.preventDefault();
-        $('.second_part').slideDown(500);
+        $('.info-block__second_part').slideDown(500);
 		$('.more-info').toggleClass('more-info_active');
     });
 }); 
