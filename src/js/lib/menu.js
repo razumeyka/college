701d966069b9eb7ffe5ae5031c864cbbdb749bce
razumeyka <33911265+razumeyka@users.noreply.github.com>
2018-12-17 17:45:57 +0200

    final corrections

---
+++ src/js/lib/menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+// mobile_menu
+    
+    $('.burger').click( function() { 
+        $('header .menu').slideToggle(300);
+        $('.burger').toggleClass( 'burger_active' ); 
+    });
+    
+// sub-menu 
+
+    $('.menu li.menu-item-has-children>a' ).click(function(e){
+		if(!$('header .burger').is(':visible'))return;
+		e.preventDefault();
+		$('.sub-menu').not($(this).closest('li').find('.sub-menu')).slideUp('300');
+		$(this).closest('li').find('.sub-menu').slideToggle('300');
+	});
+    
+}); 
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,8 @@
 @@include('./lib/jquery.fancybox.min.js')
 @@include('./lib/slider.js')
+@@include('./lib/menu.js')
 
 $(document).ready(function(){
-    
-// mobile_menu
-    
-    $('.burger').click( function() { 
-        $('.header2 .menu').slideToggle(300);
-        $('.burger').toggleClass( 'burger_active' ); 
-    });
-    
-// sub-menu 
-
-    $('.menu li.menu-item-has-children>a').click(function(e){
-        if($('.burger').is(':visible')){
-            e.preventDefault();
-            $(this).closest('li').find('.sub-menu').slideToggle(300);
-        }
-    }); 
 
 // hidden text
     $(".info-block__second-part").slideUp(0);
